256c04155deb6e4eef01253055ac118216558001
Jkoury997 <jorge_koury@me.com>
2023-10-02 07:47:16 -0300

    ACTUALIZACION

--- mouckupPaginas/prueba.js
+++ mouckupPaginas/prueba.js
@@ -1,6 +1,7 @@
 function generateDateRanges(dateTo) {
     const ranges = [];
     let currentDate = new Date(dateTo);
+    const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
     for (let i = 0; i < 12; i++) {
         // Obtener el último día del mes actual
@@ -10,6 +11,7 @@ function generateDateRanges(dateTo) {
         let firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
 
         ranges.push({
+            month: monthNames[currentDate.getMonth()],
             dateFrom: firstDayOfMonth.toISOString().split('T')[0],  // Convertir a formato YYYY-MM-DD
             dateTo: lastDayOfMonth.toISOString().split('T')[0]
         });
@@ -20,6 +22,5 @@ function generateDateRanges(dateTo) {
 
     return ranges;
 }
-
 
 console.log(generateDateRanges("2023-09-29"));
