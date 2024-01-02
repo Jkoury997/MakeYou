2cb6df2a038731817b652c8ee4e353e62ba988f6
Jkoury997 <jorge_koury@me.com>
2024-01-01 21:39:23 -0300

    add

--- client2/src/components/ScannerQR/index.jsx
+++ client2/src/components/ScannerQR/index.jsx
@@ -1,52 +1,50 @@
-import { Component } from 'react';
+import { useEffect,useState } from 'react';
 import { Html5QrcodeScanner } from 'html5-qrcode';
 
-class ScannerQR extends Component {
-  componentDidMount() {
-    const config = {
+const ScannerQR = () => {
+
+  const [qrcode , setQrCode] = useState()
+  useEffect(() => {
+    const qrScannerConfig = {
       fps: 10,
       qrbox: 250,
-      facingMode: "environment" // Cambiado a cámara trasera
+      facingMode: "environment",
     };
 
-    const onScanSuccess = (decodedText, decodedResult) => {
-      console.log(`Código QR detectado: ${decodedText}`, decodedResult);
-      this.showAlert(decodedText); // Muestra una alerta con el texto decodificado
+    const handleScanSuccess = (decodedText) => {
+      // Redirigir al usuario a la página deseada
+      setQrCode(decodedText)
+
     };
 
-    const onScanFailure = (error) => {
-      console.log(`Error al escanear: ${error}`);
+    const handleScanFailure = (error) => {
+      console.error(`Error scanning QR Code: ${error}`);
     };
 
-    this.scanner = new Html5QrcodeScanner(
-      "qr-reader", config, /* verbose= */ false);
-    this.scanner.render(onScanSuccess, onScanFailure);
-  }
+    const qrScanner = new Html5QrcodeScanner(
+      "qr-reader", qrScannerConfig, false
+    );
+    qrScanner.render(handleScanSuccess, handleScanFailure);
 
-  showAlert(decodedText) {
-    // Puedes reemplazar esto con un modal de Bootstrap para una mejor UX
-    alert(`Código QR detectado: ${decodedText}`);
-  }
+    return () => {
+      qrScanner.clear();
+    };
+  }, []);
 
-  componentWillUnmount() {
-    this.scanner.clear();
-  }
-
-  render() {
-    return (
-      <div className="container my-4">
-        <div className="row justify-content-center">
-          <div className="col-md-6">
-            <h2 className="text-center mb-4">Escaner de Códigos QR</h2>
-            <div id="qr-reader" className="mb-4 border"></div>
-            <p className="text-center">
-              Apunta la cámara trasera hacia un código QR para escanearlo.
-            </p>
-          </div>
+  return (
+    <div className="container my-4">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <h2 className="text-center mb-4">QR Code Scanner</h2>
+          <div id="qr-reader" className="mb-4 border"></div>
+          <p className="text-center">
+            Point the rear camera at a QR Code to scan it.
+          </p>
+          <p>{qrcode} </p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default ScannerQR;
